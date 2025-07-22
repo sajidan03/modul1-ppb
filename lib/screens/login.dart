d76ddb3a5a9e549f025789e3dae5bde469369a64
sajidan03 <rifansyahsajidan03@gmail.com>
2025-07-22 09:48:37 +0700

    sajidan

---
+++ lib/screens/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
